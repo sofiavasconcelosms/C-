62bdc6ec5fb272e1e9f80bb605659e518d26f245
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-09-04 14:27:05 -0300

    Add files via upload

---
+++ MinhaAplicacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;//biblioteca
+using System.Collections.Generic;//biblioteca
+using System.Linq;//biblioteca
+using System.Text;//biblioteca
+using System.Threading.Tasks;//biblioteca
+
+namespace MinhaAplicacao //nome da aplicação criada
+{
+    internal class Program //classe principal
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola Mundo!");//Aqui imprime na tela uma informação
+            Console.WriteLine("Agora posso começar");
+            Console.ReadLine();
+            Console.WriteLine(10+6);
+            Console.ReadLine();
+        }
+    }
+}
