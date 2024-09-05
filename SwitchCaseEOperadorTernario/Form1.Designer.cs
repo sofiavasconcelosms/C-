a1439b15f6c05b33f9f7c65b934499c293e41fb7
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-09-05 17:51:15 -0300

    Add files via upload

---
+++ SwitchCaseEOperadorTernario/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace SwitchCaseEOperadorTernario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(278, 143);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(88, 25);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(113, 147);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(16, 16);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "...";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(151, 12);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 22);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.ForeColor = System.Drawing.Color.IndianRed;
+            this.btnFechar.Location = new System.Drawing.Point(334, 314);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(88, 25);
+            this.btnFechar.TabIndex = 3;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 351);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ SwitchCaseEOperadorTernario/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchCaseEOperadorTernario
+{
+    public partial class Form1 : Form
+    {
+
+        int valor=10;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //CONDIÇAO IF
+            valor = int.Parse(txtNumero.Text);
+
+            //if (valor==10)
+            //{
+            //    lblResultado.Text = "é 10";
+            //}
+            //else if (valor == 7)
+            //{
+            //    lblResultado.Text = "É 7";
+            //}
+            //else if(valor >= 50)
+            //{
+            //    lblResultado.Text = "Maior ou igual a 50";
+            //}
+            //else
+            //{
+            //    lblResultado.Text = "foi nenhuma";
+            //}
+
+
+
+            //switch(valor)
+            //{
+            //    case 10: //caso for dez -> 1 opçao
+            //        lblResultado.Text = "É 10";
+            //        //intruções
+            //        break;
+            //    case 7:
+            //        lblResultado.Text = "É 7";
+            //        break;
+            //    case 50:
+            //        lblResultado.Text = "É 50";
+            //        break;
+            //    default:
+            //        lblResultado.Text = "Não foi achado";
+            //        break;
+            //}
+
+
+            //Operador ternario
+
+            lblResultado.Text = valor == 10 ? "SIMM" : "NÃOO"; 
+
+            //if(valor == 10)
+            //{
+            //    lblResultado.Text = "Sim";
+            //}
+            //else{
+            //    lblResultado.Text = "não";
+            //}
+        }
+
+       
+    }
+}
