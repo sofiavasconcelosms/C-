2c99b7712985799df96a80c8799266abd00c0219
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-09-04 21:05:32 -0300

    Add files via upload

---
+++ MeuSistema/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace MeuSistema
+{
+    partial class FrmMeuSistema
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.txtDigite = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(29, 249);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(250, 249);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(86, 23);
+            this.btnCancelar.TabIndex = 1;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtFrase.Location = new System.Drawing.Point(29, 203);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(307, 22);
+            this.txtFrase.TabIndex = 2;
+            this.txtFrase.Text = "Digite seu nome";
+
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnSair.Location = new System.Drawing.Point(680, 456);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(86, 23);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnFechar.Location = new System.Drawing.Point(29, 456);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(86, 23);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // txtDigite
+            // 
+            this.txtDigite.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtDigite.Location = new System.Drawing.Point(29, 39);
+            this.txtDigite.Name = "txtDigite";
+            this.txtDigite.Size = new System.Drawing.Size(307, 22);
+            this.txtDigite.TabIndex = 5;
+            this.txtDigite.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // FrmMeuSistema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 504);
+            this.Controls.Add(this.txtDigite);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk);
+            this.Name = "FrmMeuSistema";
+            this.Text = "Aplicação";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.TextBox txtDigite;
+    }
+}
+
---
+++ MeuSistema/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeuSistema
+{
+    public partial class FrmMeuSistema : Form
+    {
+        string meuNome = "";//texto
+
+        //modificador //tipo //nome
+        public string nome;
+        private int valor;
+        protected string senha;
+
+        //mais usados 
+        int numeros = 0;
+        string endereco = "Digite seu end.";
+        bool estarPago;
+        double valores;
+        decimal vPago;
+
+        public FrmMeuSistema()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //txtFrase.Text = "Sofia Vasconcelos";
+
+            meuNome = txtDigite.Text;
+
+            txtFrase.Text = txtDigite.Text;
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtFrase.Text = "";
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
