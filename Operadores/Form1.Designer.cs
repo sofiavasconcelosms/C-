f14f0c7e2666b15d010a58fc2f0821c1f44a4915
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-09-05 16:57:56 -0300

    Add files via upload

---
+++ Operadores/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Operadores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNota4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(164, 56);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(163, 22);
+            this.txtNota1.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(361, 311);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nota  2";
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(164, 103);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(163, 22);
+            this.txtNota2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota 3";
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.Location = new System.Drawing.Point(164, 161);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(163, 22);
+            this.txtNota3.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Nota 5";
+            // 
+            // txtNota4
+            // 
+            this.txtNota4.Location = new System.Drawing.Point(164, 220);
+            this.txtNota4.Name = "txtNota4";
+            this.txtNota4.Size = new System.Drawing.Size(163, 22);
+            this.txtNota4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Média";
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(164, 312);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.Size = new System.Drawing.Size(163, 22);
+            this.txtMedia.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nota 1";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.ForeColor = System.Drawing.Color.Firebrick;
+            this.btnFechar.Location = new System.Drawing.Point(410, 389);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(48, 23);
+            this.btnFechar.TabIndex = 13;
+            this.btnFechar.Text = "Sair";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(77, 371);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Situação";
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Location = new System.Drawing.Point(161, 371);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(0, 16);
+            this.lblSituacao.TabIndex = 15;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 424);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNota4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNota1);
+            this.Name = "Form1";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNota4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSituacao;
+    }
+}
+
---
+++ Operadores/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operadores
+{
+    public partial class Form1 : Form
+    {
+
+        //Declara variaveis 
+
+        float not1, not2, not3, not4;
+        float media;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            not1 = float.Parse(txtNota1.Text);
+            not2 = float.Parse(txtNota2.Text);
+            not3 = float.Parse(txtNota3.Text);
+            not4 = float.Parse(txtNota4.Text);
+
+            float soma = not1 +not2 + not3 + not4;
+
+            media = soma/4;
+
+            txtMedia.Text = Convert.ToString(media);
+
+            //condição IF
+
+            if (media == 7){
+                lblSituacao.Text = "APROVADO NA MARRA";
+            }
+            else if (media > 7 && media < 10)
+            {
+                lblSituacao.Text = "APROVADO";
+
+            }else if(media == 10)
+            {
+                lblSituacao.Text = "PARABENS PELA NOTA";
+            }
+            else
+            {
+                lblSituacao.Text = "REPROVADO";
+            }
+
+            /*
+             Operadores:
+
+            == --> igual
+            != --> Diferende 
+            && --> e  [Ex: if(media != 7 && MEDIA == 8)]
+            || --> ou [Ex: if(media != 7 || MEDIA == 8)]
+            >  --> maior
+            <  --> menor
+            >=  --> maior ou igual
+            <=  --> menor ou igual
+
+
+            INCREMENTO / DECREMENTO
+
+            ++ --> MEDIA = ++ (media recebe mais um)
+            -- --> MEDIA = -- (media recebe menos um)
+
+            MEDIA += (Ela mesmo mais ela memso)
+            MEDIA -= (Ela menos ela mesmo)
+
+             */
+
+        }
+    }
+}
