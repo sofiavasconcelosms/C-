e5466d3d940875c4aaebe2eb573cf38b844e25cd
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-09-05 23:01:49 -0300

    Add files via upload

---
+++ ColecaoArray/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ColecaoArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(140, 354);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 16;
+            this.lista.Location = new System.Drawing.Point(58, 34);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(243, 276);
+            this.lista.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 433);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.ListBox lista;
+    }
+}
+
---
+++ ColecaoArray/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColecaoArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //ARRAYS,
+            //10,20,30,40,50
+
+            //int[] valores;
+            //valores = new int[5]; //de indice 5 --> sempre começa com o indice 0
+            //valores[0] = 10;
+            //valores[1] = 20;
+            //valores[2] = 30;
+            //valores[3] = 40;
+            //valores[4] = 50;
+
+            //lista.Items.Add(valores[2]);
+
+
+            //int[] valores = new int[] { 10, 20, 30, 40, 50, 60 }; // de zero a seis
+
+            //foreach(int valor in valores)
+            //    lista.Items.Add(valor);
+
+
+            //int[] valores = new int[] { 10, 20, 30, 40, 50, 60 };
+            //for (int indice = 0; indice <= 5; indice++) 
+            //{ 
+            //    lista.Items.Add(valores[indice]);
+            //}
+
+
+            int[] valores = new int[] { 10, 20, 30, 40, 50, 60, 20, 30, 40, 50, 60, 20, 30, 40, 50, 60, 70 };
+            for (int indice = 0; indice< valores.Length/*comprimento*/; indice++)
+                lista.Items.Add(valores[indice]);
+            
+        }
+    }
+}
