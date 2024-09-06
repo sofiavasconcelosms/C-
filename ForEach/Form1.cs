480abe0bcc5eabf824a44d26ddba85bcd40f94a3
Sofia Vasconcelos Moreira e Silva <sofiavasconcelosmsilva@gmail.com>
2024-09-05 22:26:09 -0300

    Add files via upload

---
+++ ForEach/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForEach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //For Each
+
+            //string frase = "Meu curso C# na zatec cursos";
+            ////lista.Items.Add(frase.ToUpper());
+
+            ////string frase2 = "Meu nome é Sofia";
+            ////lista.Items.Add(frase2.Length); //comprimento
+
+            //foreach (char f in frase)
+            //{ // para cada caracteres de frases dentro de f 
+
+            //    lista.Items.Add(f);
+            //}
+
+
+            List<string> lista_nomes = new List<string>()
+            {
+                "leo", "jose", "alex", "alice", "bernardo", "marcia"
+            };
+
+            foreach (string ls in lista_nomes) 
+            {
+                lista.Items.Add(ls);
+            }
+
+        }
+    }
+}
